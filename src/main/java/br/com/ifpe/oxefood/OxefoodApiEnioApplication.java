bf57f0ad6144ee7bcc068b31a96faca68e77f1b4
eniobatalha <eniobatalha@gmail.com>
2024-02-28 19:07:15 -0300

    implementando estrutura inicial de pacotes

---
+++ src/main/java/br/com/ifpe/oxefood/OxefoodApiEnioApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiEnioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiEnioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifpe/oxefood/OxefoodApiEnioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OxefoodApiEnioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
